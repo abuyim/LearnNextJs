928b191450b6ec9b3bfe1f24eb4d630d92a71faa
Abubeker Yimam <AYimam@excellerentsolutions.com>
2025-06-16 17:45:12 +0300

    commnt out out route

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -30,14 +30,14 @@ import { prisma } from '@/lib/prisma';
 //   });
 // }
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  const { title, content } = await req.json();
-  const updated = await prisma.post.update({
-    where: { id: Number(params.id) },
-    data: { title, content },
-  });
-  return Response.json(updated);
-}
+// export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+//   const { title, content } = await req.json();
+//   const updated = await prisma.post.update({
+//     where: { id: Number(params.id) },
+//     data: { title, content },
+//   });
+//   return Response.json(updated);
+// }
 
 export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
   await prisma.post.delete({ where: { id: Number(params.id) } });
